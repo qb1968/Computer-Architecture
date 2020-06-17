3b50bc649626f5b12bd5295e6c5b933a5bc89458
qb1968 <qb1968@aol.com>
2020-06-16 21:03:44 -0400

    Day2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,63 +1,64 @@
-"""CPU functionality."""
-
 import sys
 
-HLT = 0b00000001
 LDI = 0b10000010
 PRN = 0b01000111
+HLT = 0b00000001
+MUL = 0b10100010
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
+        pass
         self.pc = 0
-        self.ram = [0] * 8
-        self.reg = [0] * 256
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.SP = 0xf3
 
-        self.PC = 0
-        self.IR = 0
-        self.MAR = 0
-        self.MDR = 0
-        self.FL = 0
-        
-    def ram_read(self, address):
-        return self.ram[address]
-
-    def ram_write(self, value, address):
-        self.ram[address] = value
-            
-
-        
-
-    def load(self):
-        """Load a program into ram."""
-
+    def load(self, filename):
+        """Load a program into memory."""
+        # print("Loading CPU...")
         address = 0
 
-        # For now, we've just hardcoded a program:
+        try:
+            address = 0
+            #open file
+            with open(sys.argv[1]) as f:
+                #read all line
+                for line in f:
+                    # make sure no errors occur in spacing
+                    comment_split = line.strip().split("#")
+                    # number string to int
+                    value = comment_split[0].strip()
+                    # Leave empty
+                    if value == "":
+                        continue
+                    instruction = int(value, 2)
+                    # Populate array
+                    self.ram[address] = instruction
+                    address += 1
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        except:
+            print("cant find file")
+            sys.exit(2)
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+
+    def ram_read(self, address):
+        # The MAR contains the address that is being read or written to.
+        return self.ram[address]
+
+    def ram_write(self, address, value):
+        # The MDR contains the data that was read or the data to write. 
+        self.ram[address] = value
 
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == MUL:
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -66,7 +67,6 @@ class CPU:
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
-
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
             #self.fl,
@@ -80,25 +80,27 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
-    
-        
+
     def run(self):
         """Run the CPU."""
-        logic = True
-        while logic:
-            IR = self.ram_read(self.pc)#Read memory address
-            op_a = self.ram_read(self.pc + 1)
-            op_b = self.ram_read(self.pc + 2)
-            if IR == LDI:# Read PC
-                self.reg[op_a] = op_b
+        while True:
+            IR = self.ram_read(self.pc)
+            opcode = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            # print(IR, self.pc, operand_a, operand_b)
+            if opcode == LDI:
+                self.reg[operand_a] = operand_b
+                # print(self.pc, self.reg, self.ram)
                 self.pc += 3
-            elif IR == PRN:
-                print(self.reg[op_a])
+            elif opcode == PRN:
+                print(self.reg[operand_a])
                 self.pc += 2
-            elif IR == HLT:
-                self.pc += 1
-                break
+            elif opcode == MUL:
+                self.alu(opcode, operand_a, operand_b)
+                self.pc += 3
+            elif opcode == HLT:
+                sys.exit(0)
             else:
-                self.pc += 1
-                print(f"{IR} - command is not available")
-                sys.exit()
+                print(f"Did not work")
+                sys.exit(1)
